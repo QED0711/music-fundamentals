4646e8d823be44183f52f91b4b2159562122a9cd
qed0711 <qdizon@gmail.com>
2019-01-18 08:11:00 -0500

    reorderContents as static method on Content model

--- server/models/content.js
+++ server/models/content.js
@@ -23,4 +23,55 @@ const contentSchema = new Schema({
     position: Number,
 })
 
+
+// METHODS
+
+contentSchema.statics.reorderContents = async function(lessonId, id, position, cb){
+    return new Promise(async (resolve) => {
+        let contents = await this.find({lessonId});
+        contents = contents.sort((a,b) => a.position - b.position);
+    
+        position = position > contents.length ? contents.length - 1 : position
+        let oldPosition = contents.filter(x => x.id === id)[0].position;
+        let movement = oldPosition - position;
+    
+        let newOrder = []
+    
+        if(movement > 0){ // moving position backwards
+    
+            for(let i = 0; i < contents.length; i++){
+                if(i === oldPosition) continue;
+                if(i === position){
+                    newOrder.push(contents[oldPosition]);
+                    newOrder[newOrder.length - 1].position = newOrder.length - 1
+                    await newOrder[newOrder.length - 1].save();
+                    newOrder.push(contents[i]);
+                } else {
+                    newOrder.push(contents[i]);
+                }
+                newOrder[newOrder.length - 1].position = newOrder.length - 1
+                await newOrder[newOrder.length - 1].save();
+            }
+            
+        } else if(movement < 0){ // moving forwards
+            for(let i = 0; i < contents.length; i++){
+                if(i === oldPosition) continue; 
+                if(i === position){
+                    newOrder.push(contents[i]);
+                    newOrder[newOrder.length - 1].position = newOrder.length - 1
+                    await newOrder[newOrder.length - 1].save();
+                    newOrder.push(contents[oldPosition]);
+                } else {
+                    newOrder.push(contents[i]);
+                }
+                newOrder[newOrder.length - 1].position = newOrder.length - 1
+                await newOrder[newOrder.length - 1].save();
+            }
+        }
+        resolve(newOrder[position])    
+    })
+}
+
+
+
 module.exports =  mongoose.model("Content", contentSchema);
